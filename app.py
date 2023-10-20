47e91f88539f65bb8ab6edfda5040518b07c4494
tanhh <nhoklilom2.0@gmail.com>
2023-10-20 15:55:19 +0700

    fix giao dien

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template,request,jsonify
 from flask_cors import CORS
 from chat import get_response
 
-app = Flask(__name__)
-CORS(app)
-
+app = Flask(__name__,)
+# CORS(app)
+@app.get('/')
+def index_get():
+    return render_template("base.html")
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
